acff1a224d369474b02483179814ab71b7456a26
jozwright <jozwright@gmail.com>
2014-09-15 21:42:21 -0700

    create number

--- app/controllers/phone_numbers_controller.rb
+++ app/controllers/phone_numbers_controller.rb
@@ -8,7 +8,7 @@ class PhoneNumbersController < ApplicationController
   end
 
   def create
-    @phone_number = PhoneNumber.new(phone_numbers_params)
+    @phone_number = PhoneNumber.new(phone_number_params)
     if @phone_number.save
       session[:phone_number_id] = @phone_number.id
       redirect_to root_url, notice: "The number has been logged."
@@ -22,10 +22,10 @@ class PhoneNumbersController < ApplicationController
   end
 
   def update
-    @phone_number = phone_number.find(current_phone_number.id)
-    @phone_number.update(phone_numbers_params)
+    @phone_number = phone_number.find(current_user.id)
+    @phone_number.update(phone_number_params)
     if @phone_number.valid?
-      flash[:notice] = "#{@phone_number.email} has been updated"
+      flash[:notice] = "#{@phone_number.number} has been updated"
       redirect_to root_url
     else
       render 'edit'
@@ -35,7 +35,7 @@ class PhoneNumbersController < ApplicationController
 
 private
 
-  def phone_numbers_params
-    params.require(:user).permit(:user_id, :number)
+  def phone_number_params
+    params.require(:phone_number).permit(:user_id, :number)
   end
 end
