2e24cf4bff1cf9fa87d661932e5a701b5197795c
jozwright <jozwright@gmail.com>
2014-09-17 10:20:19 -0700

    able to create confirmations

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,5 +1,26 @@
 class ConfirmationsController < ApplicationController
-	def index
-		@confirmations = Confirmation.all
-	end
+
+  def new
+    @confirmation = Confirmation.new
+  end
+
+  def create
+  	@phone_number = PhoneNumber.find(params[:phone_number_id])
+    @testimony = Testimony.find(params[:testimony_id])
+    params[:confirmation] = {testimony_id: params[:testimony_id]}
+    @confirmation = Confirmation.create(confirmation_params)
+    if @confirmation.valid?
+      flash[:notice] = "Your confirmation has been counted!"
+      Testimony.increment_counter(:tally, @confirmation.testimony_id)
+      redirect_to phone_number_path(@phone_number)
+    else
+      redirect_to phone_number_path(@phone_number)
+    end
+  end
+
+private
+
+  def confirmation_params
+    params.require(:confirmation).permit(:testimony_id, :user_id).merge(:user_id => current_user.id)
+  end
 end
